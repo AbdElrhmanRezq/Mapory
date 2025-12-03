41091424bf8fac3dc36529902817a174bbe513a6
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-03 22:43:19 +0200

    Add image upload on profile image tap
    Wraps the profile image in a GestureDetector to allow users to upload a new profile image when tapped. After uploading, user data is refreshed to reflect changes.

--- lib/features/profile/presentation/screens/widgets/profile_image.dart
+++ lib/features/profile/presentation/screens/widgets/profile_image.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mapory/consts.dart';
 import 'package:mapory/features/profile/data/models/user_model.dart';
+import 'package:mapory/features/profile/presentation/cubit/user_data_cubit/user_data_cubit.dart';
+import 'package:mapory/features/profile/presentation/cubit/user_images_cubit/user_images_cubit.dart'
+    show UserImagesCubit;
 
 class ProfileImage extends StatelessWidget {
   const ProfileImage({
@@ -19,16 +23,24 @@ class ProfileImage extends StatelessWidget {
     return Positioned(
       top: height * 0.23,
       left: width * 0.333,
-      child: Container(
-        decoration: BoxDecoration(
-          color: KMainBackground,
-          borderRadius: BorderRadius.circular(100),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            radius: 59,
-            backgroundImage: NetworkImage(user.profileImage),
+      child: GestureDetector(
+        onTap: () async {
+          await BlocProvider.of<UserImagesCubit>(
+            context,
+          ).uploadUserImage('profile_image');
+          await BlocProvider.of<UserDataCubit>(context).fetchUserData();
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: KMainBackground,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 59,
+              backgroundImage: NetworkImage(user.profileImage),
+            ),
           ),
         ),
       ),
