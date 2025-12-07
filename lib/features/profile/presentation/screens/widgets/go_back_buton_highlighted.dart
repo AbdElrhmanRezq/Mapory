9f826410b0cbb5334fe239534cd208b8a59cde29
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-08 00:52:58 +0200

    Add GoBackButtonHighlighted widget
    Introduces a reusable GoBackButtonHighlighted widget for profile screens, providing a styled circular back button using GoRouter for navigation.

---
+++ lib/features/profile/presentation/screens/widgets/go_back_buton_highlighted.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mapory/consts.dart';
+
+class GoBackButtonHighlighted extends StatelessWidget {
+  const GoBackButtonHighlighted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Color.fromRGBO(181, 181, 181, 0.3),
+      child: IconButton(
+        onPressed: () {
+          GoRouter.of(context).pop();
+        },
+        icon: Icon(Icons.arrow_back_ios_new, color: KMainColor),
+      ),
+    );
+  }
+}
