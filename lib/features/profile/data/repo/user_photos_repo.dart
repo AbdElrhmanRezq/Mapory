49a2c6ec4a18c2b3e2841ea2e5b8ab61ddc08b6d
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-10 21:47:48 +0200

    Add getPhotosLikes method to UserPhotosRepoImpl
    Introduces the getPhotosLikes method to fetch users who liked a specific photo by its ID. This method queries the 'likes' table and maps the results to UserModel instances.

--- lib/features/profile/data/repo/user_photos_repo.dart
+++ lib/features/profile/data/repo/user_photos_repo.dart
@@ -1,9 +1,7 @@
 import 'package:mapory/features/home/data/models/photo_model.dart';
+import 'package:mapory/features/profile/data/models/user_model.dart';
 
 abstract class UserPhotosRepo {
   Future<int> getUserPhotosCount({String visibility = 'all'});
-  Future<List<PhotoModel>> getUserPhotos({
-    required int limit,
-    required int offset,
-  });
+  Future<List<UserModel>> getPhotosLikes({required String photoId});
 }
--- lib/features/profile/data/repo/user_photos_repo_impl.dart
+++ lib/features/profile/data/repo/user_photos_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:mapory/core/utils/service_locator.dart';
 import 'package:mapory/features/home/data/models/photo_model.dart';
+import 'package:mapory/features/profile/data/models/user_model.dart';
 import 'package:mapory/features/profile/data/repo/user_photos_repo.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -27,11 +28,18 @@ class UserPhotosRepoImpl implements UserPhotosRepo {
   }
 
   @override
-  Future<List<PhotoModel>> getUserPhotos({
-    required int limit,
-    required int offset,
-  }) {
-    // TODO: implement getUserPhotos
-    throw UnimplementedError();
+  Future<List<UserModel>> getPhotosLikes({required String photoId}) async {
+    final supabase = getIt<SupabaseClient>();
+    final data = await supabase
+        .from('likes')
+        .select('users(*)')
+        .eq('p_id', photoId);
+
+    final List<UserModel> users = [];
+    final records = data as List<dynamic>;
+    records.forEach((record) {
+      users.add(UserModel.fromMap(record));
+    });
+    return users;
   }
 }
