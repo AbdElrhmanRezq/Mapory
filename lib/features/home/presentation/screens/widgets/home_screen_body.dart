5d6b1083bd1d13964f27b52e3079b0793f52e29f
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-19 03:45:09 +0200

    Modified Home Screen Body to the cubit

--- lib/features/home/presentation/screens/widgets/home_screen_body.dart
+++ lib/features/home/presentation/screens/widgets/home_screen_body.dart
@@ -1,7 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapory/consts.dart';
+import 'package:mapory/core/utils/app_router.dart';
+import 'package:mapory/features/home/presentation/cubit/map_cubit/map_cubit.dart';
+import 'package:mapory/features/home/presentation/cubit/map_cubit/map_state.dart';
 
 class HomeScreenBody extends StatefulWidget {
   const HomeScreenBody({super.key});
@@ -11,41 +15,45 @@ class HomeScreenBody extends StatefulWidget {
 }
 
 class HomeScreenBodyState extends State<HomeScreenBody> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-    bearing: 192.8334901395799,
-    target: LatLng(37.43296265331129, -122.08832357078792),
-    tilt: 59.440717697143555,
-    zoom: 19.151926040649414,
-  );
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+    return BlocProvider(
+      create: (_) => MapCubit()..init(context),
+      child: BlocBuilder<MapCubit, MapState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: GoogleMap(
+              style: state.mapStyle,
+              myLocationEnabled: state.permissionGranted,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(30.0444, 31.2357),
+                zoom: 12,
+              ),
+              markers: state.markers,
+              onMapCreated: (controller) {
+                context.read<MapCubit>().setController(controller);
+              },
+            ),
+          );
         },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
       ),
     );
   }
+}
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+class ProfileButton extends StatelessWidget {
+  const ProfileButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Color.fromRGBO(181, 181, 181, 0.3),
+      child: IconButton(
+        onPressed: () {
+          GoRouter.of(context).push(AppRouter.kProfile);
+        },
+        icon: Icon(Icons.person, color: KMainColor),
+      ),
+    );
   }
 }
