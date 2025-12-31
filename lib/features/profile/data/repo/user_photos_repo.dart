b70d58591241a4ba0a99b01e52d656381871d833
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-31 11:10:26 +0200

    Added Create memory functionality

--- lib/features/profile/data/repo/user_photos_repo.dart
+++ lib/features/profile/data/repo/user_photos_repo.dart
@@ -1,6 +1,13 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mapory/features/home/data/models/photo_model.dart';
 import 'package:mapory/features/profile/data/models/user_model.dart';
 
 abstract class UserPhotosRepo {
   Future<List<UserModel>> getPhotosLikes({required String photoId});
+  Future<void> createMemory({
+    required List<String> urls,
+    required String caption,
+    required LatLng position,
+    String visibility = "private",
+  });
 }
--- lib/features/profile/data/repo/user_photos_repo_impl.dart
+++ lib/features/profile/data/repo/user_photos_repo_impl.dart
@@ -1,3 +1,4 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mapory/core/utils/service_locator.dart';
 import 'package:mapory/features/home/data/models/photo_model.dart';
 import 'package:mapory/features/profile/data/models/user_model.dart';
@@ -19,5 +20,40 @@ class UserPhotosRepoImpl implements UserPhotosRepo {
       users.add(UserModel.fromMap(record));
     });
     return users;
+  }
+
+  @override
+  Future<void> createMemory({
+    required List<String> urls,
+    required String caption,
+    required LatLng position,
+    String visibility = "private",
+  }) async {
+    final supabase = getIt<SupabaseClient>();
+    final userId = supabase.auth.currentUser!.id;
+    final data = await supabase
+        .from('memories')
+        .insert({
+          'u_id': userId,
+          'caption': caption,
+          'lat': position.latitude,
+          'long': position.longitude,
+          'created_at': DateTime.now().toIso8601String(),
+          'visibility': visibility,
+        })
+        .select()
+        .single();
+    await supabase
+        .from('photos')
+        .insert(
+          urls.map((url) {
+            return {
+              'u_id': userId,
+              'image_url': url,
+              'created_at': DateTime.now().toIso8601String(),
+              'm_id': data['m_id'],
+            };
+          }).toList(),
+        );
   }
 }
