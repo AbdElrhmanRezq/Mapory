7128fc2959bf4d39ff2943f9dae6b8552c6c7ab4
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2026-01-12 16:41:32 +0200

    Add CommentsRepo interface and Supabase implementation
    Introduces the CommentsRepo abstract class for comment operations and provides a Supabase-based implementation (CommentsRepoImpl) for fetching and adding comments related to a memory.

---
+++ lib/features/home/data/repo/comments_repo.dart
@@ -0,0 +1,13 @@
+import 'package:mapory/features/home/data/models/comment_model.dart';
+
+abstract class CommentsRepo {
+  Future<List<CommentModel>> fetchComments({
+    required String memoryId,
+    int limit = 10,
+    int offset = 0,
+  });
+  Future<void> addComment({
+    required String memoryId,
+    required CommentModel comment,
+  });
+}
---
+++ lib/features/home/data/repo/comments_repo_impl.dart
@@ -0,0 +1,36 @@
+import 'package:mapory/core/utils/service_locator.dart';
+import 'package:mapory/features/home/data/models/comment_model.dart';
+import 'package:mapory/features/home/data/repo/comments_repo.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class CommentsRepoImpl implements CommentsRepo {
+  @override
+  Future<void> addComment({
+    required String memoryId,
+    required CommentModel comment,
+  }) async {
+    final SupabaseClient supabase = getIt<SupabaseClient>();
+
+    await supabase.from('comments').insert({
+      'm_id': memoryId,
+      'u_id': comment.uId,
+      'text': comment.text,
+    });
+  }
+
+  @override
+  Future<List<CommentModel>> fetchComments({
+    required String memoryId,
+    int limit = 10,
+    int offset = 0,
+  }) async {
+    final SupabaseClient supabase = getIt<SupabaseClient>();
+    final response = await supabase
+        .from('comments')
+        .select()
+        .eq('m_id', memoryId)
+        .range(offset, offset + limit - 1);
+
+    return response.map((json) => CommentModel.fromJson(json)).toList();
+  }
+}
